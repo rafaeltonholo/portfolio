68551fcb7a7b5b23f95184906f64e61cbcaacb13
Rafael Tonholo <rafael@tonholo.dev>
2024-04-06 15:49:04 -0300

    chore: base kobweb structure

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+// https://docs.gradle.org/current/userguide/declaring_dependencies.html#sec:type-safe-project-accessors
+enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
+
+pluginManagement {
+    repositories {
+        mavenCentral()
+        gradlePluginPortal()
+        google()
+        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+        maven("https://us-central1-maven.pkg.dev/varabyte-repos/public")
+    }
+}
+
+rootProject.name = "portfolio"
+
+include(":site")
+
