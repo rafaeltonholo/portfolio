591fb50b05d1f516232ffcf1480ec61f7cd25ac5
Rafael Tonholo <rafael@tonholo.dev>
2024-05-13 16:56:13 -0300

    fix: css color scheme

--- core/src/jsMain/kotlin/dev/tonholo/portfolio/core/ui/theme/Theme.kt
+++ core/src/jsMain/kotlin/dev/tonholo/portfolio/core/ui/theme/Theme.kt
@@ -6,7 +6,11 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.ReadOnlyComposable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
+import com.varabyte.kobweb.compose.ui.Modifier
+import com.varabyte.kobweb.compose.ui.styleModifier
 import com.varabyte.kobweb.core.KobwebApp
+import com.varabyte.kobweb.silk.init.InitSilk
+import com.varabyte.kobweb.silk.init.InitSilkContext
 import com.varabyte.kobweb.silk.init.setSilkWidgetVariables
 import com.varabyte.kobweb.silk.prepareSilkFoundation
 import com.varabyte.kobweb.silk.theme.colors.ColorMode
@@ -16,6 +20,7 @@ import dev.tonholo.portfolio.core.ui.theme.typography.LocalTypography
 import dev.tonholo.portfolio.core.ui.theme.typography.Typography
 import kotlinx.browser.document
 import kotlinx.browser.localStorage
+import kotlinx.browser.window
 
 private const val COLOR_MODE_KEY = "portfolio:colorMode"
 
@@ -23,6 +28,24 @@ data class ThemedValue<T>(
     val light: T,
     val dark: T,
 )
+
+@InitSilk
+fun initCssColorScheme(context: InitSilkContext) {
+    context.stylesheet.registerStyle("[data-theme=\"light\"]") {
+        base {
+            Modifier.styleModifier {
+                property("color-scheme", "light")
+            }
+        }
+    }
+    context.stylesheet.registerStyle("[data-theme=\"dark\"]") {
+        base {
+            Modifier.styleModifier {
+                property("color-scheme", "dark")
+            }
+        }
+    }
+}
 
 @Composable
 fun Theme(
@@ -36,6 +59,9 @@ fun Theme(
             val colorMode by ColorMode.currentState
             LaunchedEffect(colorMode) {
                 localStorage.setItem(COLOR_MODE_KEY, colorMode.name)
+                document
+                    .documentElement
+                    ?.setAttribute("data-theme", colorMode.name.lowercase())
             }
 
             val (colorScheme, elevations) = when (colorMode) {
