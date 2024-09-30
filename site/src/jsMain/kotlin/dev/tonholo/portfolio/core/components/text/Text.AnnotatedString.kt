902f906897fc354e2782def086fe336eee06e5f5
Rafael Tonholo <rafael@tonholo.dev>
2024-09-30 11:34:19 -0300

    feat: add annotated string nested range rendering

--- site/src/jsMain/kotlin/dev/tonholo/portfolio/core/components/text/Text.AnnotatedString.kt
+++ site/src/jsMain/kotlin/dev/tonholo/portfolio/core/components/text/Text.AnnotatedString.kt
@@ -1,12 +1,19 @@
 package dev.tonholo.portfolio.core.components.text
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.key
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import com.varabyte.kobweb.compose.foundation.layout.Arrangement
 import com.varabyte.kobweb.compose.foundation.layout.Column
 import com.varabyte.kobweb.compose.foundation.layout.ColumnDefaults
 import com.varabyte.kobweb.compose.ui.Alignment
 import com.varabyte.kobweb.compose.ui.Modifier
+import com.varabyte.kobweb.compose.ui.modifiers.fontWeight
+import com.varabyte.kobweb.compose.ui.thenIf
 import dev.tonholo.portfolio.core.ui.text.AnnotatedString
 import dev.tonholo.portfolio.core.ui.text.ParagraphStyle
 import dev.tonholo.portfolio.core.ui.text.SpanStyle
@@ -26,25 +33,90 @@ fun Text(
         verticalArrangement = verticalArrangement,
         horizontalAlignment = horizontalAlignment,
     ) {
-        key(text) {
-            text.styles.forEach { range ->
-                key(range) {
-                    val annotatedText = text.subSequence(range.start, range.end)
-                    when (val style = range.item) {
-                        is ParagraphStyle -> Paragraph(text = annotatedText.text)
-                        is SpanStyle -> Text(text = annotatedText.text)
-                        is TitleStyle -> Title(
-                            text = annotatedText.text,
-                            style = style,
-                        )
-
-                        is SubtitleStyle -> Text(
-                            text = annotatedText.text,
-                            style = Theme.typography.titleSmall,
-                        )
+        key(text.text) {
+            val styles = remember(text) { text.calculatedNestedStyles() }
+            SideEffect {
+                console.log("styles", styles)
+            }
+            styles.forEach { (parentRange, nestedRanges) ->
+                key(parentRange) {
+                    when {
+                        nestedRanges.isEmpty() -> RangeRenderer(text = text, range = parentRange)
+                        else -> NestedRangeRenderer(parentRange, nestedRanges, text)
                     }
                 }
             }
+        }
+    }
+}
+
+@Composable
+private fun NestedRangeRenderer(
+    parentRange: AnnotatedString.Range<out AnnotatedString.Style>,
+    nestedRanges: List<AnnotatedString.Range<out AnnotatedString.Style>>,
+    text: AnnotatedString
+) {
+    when (parentRange.item) {
+        is ParagraphStyle -> Paragraph {
+            var startRange by remember { mutableIntStateOf(parentRange.start) }
+            for (nestedRange in nestedRanges) {
+                when {
+                    nestedRange.start > startRange -> {
+                        Text(
+                            text = text.subSequence(startRange, nestedRange.start).text,
+                            disablePreWrap = true,
+                        )
+                        startRange = nestedRange.end
+                        RangeRenderer(text, nestedRange)
+                    }
+
+                    nestedRange.start == startRange -> {
+                        RangeRenderer(text, nestedRange)
+                        startRange = nestedRange.end
+                    }
+                }
+            }
+            if (startRange < parentRange.end) {
+                Text(
+                    text = text.subSequence(startRange, parentRange.end).text,
+                    disablePreWrap = true,
+                )
+            }
+        }
+
+        else -> Unit
+    }
+}
+
+@Composable
+private fun RangeRenderer(
+    text: AnnotatedString,
+    range: AnnotatedString.Range<out AnnotatedString.Style>,
+) {
+    key(range) {
+        val annotatedText = remember(text, range) { text.subSequence(range.start, range.end) }
+        when (val style = range.item) {
+            is ParagraphStyle -> Paragraph(text = annotatedText.text)
+            is SpanStyle -> Text(
+                text = annotatedText.text,
+                modifier = Modifier
+                    .thenIf(
+                        style.fontWeight != null,
+                        Modifier.fontWeight(requireNotNull(style.fontWeight)),
+                    ),
+                disablePreWrap = true,
+            )
+
+            is TitleStyle -> Title(
+                text = annotatedText.text,
+                style = style,
+            )
+
+            is SubtitleStyle -> Text(
+                text = annotatedText.text,
+                style = Theme.typography.titleSmall,
+                disablePreWrap = true,
+            )
         }
     }
 }
